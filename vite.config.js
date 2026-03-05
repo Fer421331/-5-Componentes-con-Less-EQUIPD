139e1b0d01b441862edbe2e0f06100739f3c1a96
Fer421331 <fernandogofu04@gmail.com>
2026-03-04 18:00:46 -0600

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+   base: "/-5-Componentes-con-Less-EQUIPD/",
   build: {
     rollupOptions: {
       input: {
